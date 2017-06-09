b7936aaaae2bd92ebfeb8398823c82db7e888cc2
Bryan Hughes <bryan@nebri.us>
2017-06-09 15:16:01 -0700

    Created stage 2 server code

--- solutions/stage2/server/src/index.ts
+++ solutions/stage2/server/src/index.ts
@@ -1,10 +1,34 @@
+import { json } from 'body-parser';
 import * as express from 'express';
 import { resolve } from 'path';
+import * as sqlite3 from 'sqlite3';
+
+const TABLE_NAME = 'notes';
 
 const app = express();
 
-app.use(express.static(resolve(__dirname, '..', '..', 'client', 'dist')));
+sqlite3.verbose();
+const db = new sqlite3.Database(':memory:');
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.use(express.static(resolve(__dirname, '..', '..', 'client', 'dist')));
+app.use(json());
+
+app.get('/api/notes', (req, res) => {
+  db.all(`SELECT * FROM ${TABLE_NAME}`, (err, rows) => {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  db.run(`INSERT INTO ${TABLE_NAME} (title, value) VALUES ("${req.body.title}", "${req.body.value}")`);
+});
+
+db.run(`CREATE TABLE ${TABLE_NAME} (title TEXT NOT NULL, value TEXT NOT NULL)`, (err) => {
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
 });
